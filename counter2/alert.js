3c142112cd62ef81a2fd9b6ac6ceaf82633a057d
terry-man-com <terry.man.com@icloud.com>
2024-12-04 23:57:49 +0900

    test: 😃#004 script.jsの追記)

---
+++ counter2/alert.js
@@ -0,0 +1,3 @@
+// addEventListener("load", () => {
+//   alert("ボタンを押してね❤️");
+// });
--- counter2/script.js
+++ counter2/script.js
@@ -1,3 +1,4 @@
+// カウンタープログラム
 (() => {
   const $counter = document.getElementById("js-counter");
 
@@ -5,14 +6,35 @@
     const $targetButton = e.currentTarget;
     let currentCount = parseInt($counter.textContent);
     if($targetButton.textContent === "+"){
+      if (currentCount === 5){
+        changeDarkMode();
+      }
+      if(currentCount === 10){
+        $counter.textContent = 0;
+        return;
+      }
       $counter.textContent = currentCount + 1;
     } else {
+      if (currentCount === -10) {
+        $counter.textContent = "0";
+        return;
+      }
       $counter.textContent = currentCount - 1;
     }
   }
+
 
   for (let index = 0; index < document.getElementsByClassName("js-button").length; index++) {
     document.getElementsByClassName("js-button")[index].addEventListener("click", (e) => clickHandler(e));
   }
 })();
+// 5カウント時背景を変更
+const changeDarkMode = () => {
 
+  const $counter = document.getElementById("counter");
+  console.log($counter);
+  const $counterNumber = document.getElementById("counter-number");
+  $counter.classList = "counter-second";
+  $counterNumber.classList = "counter-number-second";
+
+};
