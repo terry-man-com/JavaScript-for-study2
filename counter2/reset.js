9aae718491b5a94232bab974a9213e7359e77ff1
terry-man-com <terry.man.com@icloud.com>
2024-12-05 22:33:13 +0900

    resetBackGround関数の実装

--- counter2/reset.js
+++ counter2/reset.js
@@ -1,8 +1,11 @@
+import { resetBackGroundColor } from "./script.js";
+
 (() => {
   const $counter = document.getElementById("js-counter");
 
   const clickHandler = () => {
     $counter.textContent = 0;
+    resetBackGroundColor();
   };
 
   document.getElementById("js-reset-button").addEventListener("click", clickHandler);
--- counter2/script.js
+++ counter2/script.js
@@ -5,12 +5,13 @@
   const clickHandler = (e) => {
     const $targetButton = e.currentTarget;
     let currentCount = parseInt($counter.textContent);
-    if($targetButton.textContent === "+"){
-      if (currentCount === 5){
-        changeDarkMode();
+    if ($targetButton.textContent === "+") {
+      if (currentCount === 5) {
+        changeBackGroundColor();
       }
-      if(currentCount === 10){
+      if (currentCount === 10) {
         $counter.textContent = 0;
+        resetBackGroundColor();
         return;
       }
       $counter.textContent = currentCount + 1;
@@ -21,20 +22,41 @@
       }
       $counter.textContent = currentCount - 1;
     }
-  }
+  };
 
-
-  for (let index = 0; index < document.getElementsByClassName("js-button").length; index++) {
-    document.getElementsByClassName("js-button")[index].addEventListener("click", (e) => clickHandler(e));
+  for (
+    let index = 0;
+    index < document.getElementsByClassName("js-button").length;
+    index++
+  ) {
+    document
+      .getElementsByClassName("js-button")
+      [index].addEventListener("click", (e) => clickHandler(e));
   }
 })();
+
 // 5カウント時背景を変更
-const changeDarkMode = () => {
-
-  const $counter = document.getElementById("counter");
-  console.log($counter);
-  const $counterNumber = document.getElementById("counter-number");
-  $counter.classList = "counter-second";
-  $counterNumber.classList = "counter-number-second";
-
+const changeBackGroundColor = () => {
+  const $counter = document.getElementsByClassName("counter");
+  const $counterNumber = document.getElementsByClassName("counter-number");
+  const $button = document.getElementsByClassName("button");
+  $counter[0].classList.add("counter-second");
+  $counterNumber[0].classList.add("counter-number-second");
+  // すべてのボタンにクラスを追加する
+  for (let i = 0; i < $button.length; i++) {
+    $button[i].classList.add("button-second");
+  }
 };
+// 背景を戻す
+export const resetBackGroundColor = () => {
+  const $counter = document.getElementsByClassName("counter");
+  const $counterNumber = document.getElementsByClassName("counter-number");
+  const $button = document.getElementsByClassName("button");
+  $counter[0].classList.remove("counter-second");
+  $counterNumber[0].classList.remove("counter-number-second");
+  // すべてのボタンにクラスを追加する
+  for (let i = 0; i < $button.length; i++) {
+    $button[i].classList.remove("button-second");
+  }
+};
+
