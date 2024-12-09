f1d4bb3e09e6bce6c6c8568a8ba3ffcfef61f213
terry-man-com <terry.man.com@icloud.com>
2024-12-09 12:06:57 +0900

    add

--- Quiz/app.js
+++ Quiz/app.js
@@ -1,50 +1,67 @@
-const question = "幻影旅団の団長は？"
-const answers = [
-  'クロロ・ルシルフル',
-  'パクノダ',
-  'ジャイロ',
-  'トンパ'
+// クイズオブジェクト
+const quiz = [
+  {
+    question: "幻影旅団の団長は？",
+    answers: ["クロロ・ルシルフル", "パクノダ", "ジャイロ", "トンパ"],
+    correct: "クロロ・ルシルフル",
+  },
+  {
+    question: "湘北高校のPFは？",
+    answers: ["桜木花道", "牧伸一", "三井寿", "田岡茂一"],
+    correct: "桜木花道",
+  },
+  {
+    question: "中村政紀の好きな筋トレは？",
+    answers: ["スクワット", "スクワット", "スクワット", "ワンハンドローイング"],
+    correct: "スクワット",
+  },
 ];
 
-const correct = "クロロ・ルシルフル";
-
-document.getElementById("js-question").textContent = question;
-
-for (let i = 0; i < answers.length; i++) {
-  document.getElementsByClassName("btn-primary")[i].textContent = answers[i];
-}
+const quizLength = quiz.length;
+let quizIndex = 0;
+let score = 0;
 
 const $button = document.getElementsByClassName("btn-primary");
-$button[0].textContent = answers[0];
-$button[1].textContent = answers[1];
-$button[2].textContent = answers[2];
-$button[3].textContent = answers[3];
 
-$button[0].addEventListener('click', () => {
-  if (correct === $button[0].textContent) {
-    alert("正解！");
-  } else {
-    alert("不正解、、、");
-  }
+const setUpQuiz = () => {
+    document.getElementById("js-question").textContent = quiz[quizIndex].question;
+
+    for (let i = 0; i < $button.length; i++) {
+    $button[i].textContent = quiz[quizIndex].answers[i];
+    }
+  };
+
+
+setUpQuiz();
+
+const clickHandler = (e) => {
+    if (quiz[quizIndex].correct === e.target.textContent) {
+      alert("正解！");
+      score++;
+    } else {
+      alert("不正解、、、");
+    }
+
+    quizIndex++;
+
+    if (quizIndex < quizLength) {
+      setUpQuiz();
+    } else {
+      alert('終了！あなたの正解数は' + score + '/' + quizLength + 'です。')
+    }
+}
+for (let i = 0; i < $button.length; i++) {
+  $button[i].addEventListener("click", (e) => {
+  clickHandler(e);
 });
-$button[1].addEventListener("click", () => {
-  if (correct === $button[1].textContent) {
-    alert("正解！");
-  } else {
-    alert("不正解、、、");
-  }
-});
-$button[2].addEventListener("click", () => {
-  if (correct === $button[2].textContent) {
-    alert("正解！");
-  } else {
-    alert("不正解、、、");
-  }
-});
-$button[3].addEventListener("click", () => {
-  if (correct === $button[3].textContent) {
-    alert("正解！");
-  } else {
-    alert("不正解、、、");
-  }
-});
+}
+
+// $button[1].addEventListener("click", (e) => {
+//  clickHandler(e);
+// });
+// $button[2].addEventListener("click", (e) => {
+//   clickHandler(e);
+// });
+// $button[3].addEventListener("click", (e) => {
+//   clickHandler(e);
+// });
